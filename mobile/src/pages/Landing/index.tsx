57c97c03ed9fed11a80d4ef680d91362309eaef3
Matheus <matheusdc98@gmail.com>
2020-09-30 23:26:05 -0300

    Dia 4/4 finalizando o aplicativo

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {RectButton} from 'react-native-gesture-handler';
@@ -8,10 +8,20 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing(){
 
     const {navigate} = useNavigation();
+
+    const [totalConnections, seTotalConnections]  = useState(0);
+
+    useEffect(() =>{
+        api.get('connections').then(response => {
+            const {total} = response.data;
+            seTotalConnections(total)
+        })
+    },[]);
 
     function handleNavigationToGiveClassesPage(){
         navigate('GiveClasses');
@@ -43,7 +53,7 @@ function Landing(){
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 200 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon}/>
             </Text>
 
